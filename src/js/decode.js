53a0aca2e6a885202de4b693310fe5f01232e0b4
Kenta Yamamoto_[MBSD] <k.yamamoto@mbsd.jp>
2018-08-07 16:44:47 +0900

    prepare for apply react

---
+++ src/js/decode.js
@@ -0,0 +1,27 @@
+decode_encode = function(){
+    var text = document.getElementById("decode_text")
+    var decode_type = document.getElementById("decode_typeoption")
+    var is_encode = document.getElementById("checkbox_encode")
+    switch (decode_type) {
+        case "base64":
+            
+            break;
+        case "URL":
+            
+            break;
+        case "URLburp":
+            
+            break;
+
+        case "html":
+            
+            break;
+
+        case "unicode":
+            
+            break;
+
+        default:
+            break;
+    }
+}
