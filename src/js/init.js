a5f3fcef765e632133bc85b51549a7b0dd7796ee
gorillanet <ymk@mailyk.com>
2018-09-09 23:26:34 +0900

    json/xml整形サポート

--- src/js/init.js
+++ src/js/init.js
@@ -3,8 +3,12 @@ window.onload=function(){
     document.getElementById("button_expass").addEventListener('click', expass);
     document.getElementById("button_hidepass").addEventListener('click', hidepass);
     document.getElementById("decode_text").addEventListener('keyup', decode_encode);
+    document.getElementById("decode_text").addEventListener('keydown', decode_encode);
+    document.getElementById("decode_text").addEventListener('change', decode_encode);
+    document.getElementById("decode_typeoption").addEventListener('change', reset);
     document.getElementById("decoded_text").addEventListener('keyup', reset);
     document.getElementById("checkbox_encode").addEventListener('change', decode_encode);
     //document.getElementById("checkbox_encode").addEventListener('change', toggle_text);
-    document.getElementById("decode_typeoption").addEventListener('change', decode_encode);
+    document.getElementById("minified_json_text").addEventListener('keyup', beautify_object);
+    document.getElementById("minified_xml_text").addEventListener('keyup', beautify_xml);
 }
