d458c7b3656310b69bd1ca06e448da944054cad6
k.yamamoto <k.yamamoto@mbsd.jp>
2018-09-14 10:31:48 +0900

    webpack展開対応

--- src/js/source-map.js
+++ src/js/source-map.js
@@ -1,97 +1,70 @@
+var zip;
+var sourceMap = window.sourceMap;
 sourceMap.SourceMapConsumer.initialize({
     "lib/mappings.wasm": "js/libs/mappings.wasm"
 });
-var sourceMap = window.sourceMap;
 function xhrSuccess() { 
-    this.callback.apply(this, this.arguments); 
+  this.callback.apply(this, this.arguments); 
 }
 
 function xhrError() { 
-    console.error(this.statusText); 
+  console.error(this.statusText); 
 }
 
-function loadFile(url, callback /*, opt_arg1, opt_arg2, ... */) {
-    var xhr = new XMLHttpRequest();
-xhr.open("GET", url, true);
-xhr.onload = function (e) {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      callback(xhr.responseText);
-    } else {
-      console.error(xhr.statusText);
+function loadFile(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback(xhr.responseText);
+      } else {
+        console.error(xhr.statusText);
+      }
     }
-  }
-};
-xhr.onerror = function (e) {
-  console.error(xhr.statusText);
-};
-xhr.send(null);
+  };
+  xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+  };
+  xhr.send(null);
 }
 
-var get_text = function(){
-
+var add_zip = function(uri, content){
+  try {
+    zip.file(uri, content);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 var get_sourcemap = function(text){
-    //sourcemap.mapの取得
-      const sm = sources;
-      var consumer = new sourceMap.SourceMapConsumer(sm);
-      console.log(consumer.then(function(i){
-        for(let x of i.sources){
-          if(x.match(/.*\.(js|vue|css)$/g)){
-              //console.log(x);
-          }
+    const sm =String(text);
+    new sourceMap.SourceMapConsumer(sm)
+    .then(function(i){
+      s = i.sources;
+      console.log(i.sources[0]);
+      for(x in s){
+        if(s[x].match(/^webpack:\/{2,3}\.?\/?/g)){
+              console.log(s[x]);
+              console.log(i.sourcesContent[x]);
+              add_zip(s[x].replace(/^webpack:\/{2,3}\.?\/?/g, ""), i.sourcesContent[x]);
         }
-        console.log(i.sourcesContent[0]);
-      }));
- /*     
-    const whatever = window.sourceMap.SourceMapConsumer.with(rawSourceMap, null, consumer => {
-
-        console.log(consumer.sources);
-        // [ 'http://example.com/www/js/one.js',
-        //   'http://example.com/www/js/two.js' ]
-      
-        console.log(consumer.originalPositionFor({
-          line: 2,
-          column: 28
-        }));
-        // { source: 'http://example.com/www/js/two.js',
-        //   line: 2,
-        //   column: 10,
-        //   name: 'n' }
-      
-        console.log(consumer.generatedPositionFor({
-          source: 'http://example.com/www/js/two.js',
-          line: 2,
-          column: 10
-        }));
-        // { line: 2, column: 28 }
-      
-        consumer.eachMapping(function (m) {
-          // ...
-        });
-      
-        return computeWhatever();
-      });
-      */
+      }
+      downloader();
+    });
 }
 var downloader = function(){
-    //テスト用
-
-}
-var get_source = function(){
-  var zip = new JSZip();
-  zip.file("Hello.txt", "Hello World\n");
-  /*
-  var img = zip.folder("images");
-  img.file("smile.gif", imgData, {base64: true});
-  */
   zip.generateAsync({type:"blob"})
   .then(function(content) {
       // see FileSaver.js
       objectURL = URL.createObjectURL(content);
       browser.downloads.download({url: objectURL, saveAs: true, filename:"zip.zip"});
   });
-    var blob = new Blob([js], {type: "application/json"});
-    //loadFile("https://jsonplaceholder.typicode.com/todos/1", get_sourcemap);
+
+}
+var get_source = function(){
+    zip = new JSZip();
+    if(document.getElementById("sourcemapsrc").value){
+      loadFile(document.getElementById("sourcemapsrc").value, get_sourcemap);
+    }
 }
