a44a45946bd123bbcf8cf9b17ef4cac41618c015
k.yamamoto <k.yamamoto@mbsd.jp>
2018-09-14 15:24:05 +0900

    file名をmapファイルベースにした

--- src/js/source-map.js
+++ src/js/source-map.js
@@ -11,13 +11,24 @@ function xhrError() {
   console.error(this.statusText); 
 }
 
+var get_filename = function(f){
+  if (f) {
+    var startIndex = (f.indexOf('\\') >= 0 ? f.lastIndexOf('\\') : f.lastIndexOf('/'));
+    var filename = f.substring(startIndex);
+    if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+        filename = filename.substring(1);
+    }
+    return filename;
+  }
+}
+
 function loadFile(url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
   xhr.onload = function (e) {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        callback(xhr.responseText);
+        callback(xhr.responseText, get_filename(url));
       } else {
         console.error(xhr.statusText);
       }
@@ -37,7 +48,7 @@ var add_zip = function(uri, content){
   }
 }
 
-var get_sourcemap = function(text){
+var get_sourcemap = function(text, filename){
     const sm =String(text);
     new sourceMap.SourceMapConsumer(sm)
     .then(function(i){
@@ -50,15 +61,17 @@ var get_sourcemap = function(text){
               add_zip(s[x].replace(/^webpack:\/{2,3}\.?\/?/g, ""), i.sourcesContent[x]);
         }
       }
-      downloader();
+      downloader(filename);
     });
 }
-var downloader = function(){
+var downloader = function(filename){
   zip.generateAsync({type:"blob"})
   .then(function(content) {
       // see FileSaver.js
       objectURL = URL.createObjectURL(content);
-      browser.downloads.download({url: objectURL, saveAs: true, filename:"zip.zip"});
+      var f = filename.replace(/\.\w{1,5}$/,".zip");
+      console.log(f);
+      browser.downloads.download({url: objectURL, saveAs: true, filename:f});
   });
 
 }
